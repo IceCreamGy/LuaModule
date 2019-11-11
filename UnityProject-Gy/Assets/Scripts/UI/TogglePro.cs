9a57b2c821051ded81da16944366eb4fff05747d
GaoYuan2019 <741446970@qq.com>
2019-11-11 20:35:19 +0800

    添加签到系统。

--- UnityProject-Gy/Assets/Scripts/UI/TogglePro.cs
+++ UnityProject-Gy/Assets/Scripts/UI/TogglePro.cs
@@ -7,12 +7,51 @@ using System;
 public class TogglePro : MonoBehaviour
 {
     Toggle myToggle;
+    [HideInInspector]
     public Text Text_lable;
+    public RectTransform Rt;
+    public RectTransform RT;
     void Awake()
     {
         myToggle = GetComponent<Toggle>();
         myToggle.onValueChanged.AddListener(OnChange);
         Text_lable = transform.Find("Label").GetComponent<Text>();
+        Rt = GetComponent<RectTransform>();
+        RT = Rt;
+    }
+
+    //方便Lua端获取
+    CanvasGroup _Cg;
+    //方便开发，大小写不敏感。（瞎鸡儿堆功能，捂脸.jpg）
+    public CanvasGroup Cg
+    {
+        get
+        {
+            if (_Cg == null)    //尝试获取
+            {
+                _Cg = gameObject.GetComponent<CanvasGroup>();
+            }
+            if (_Cg == null)    //获取不到，就添加
+            {
+                _Cg = gameObject.AddComponent<CanvasGroup>();
+            }
+            return _Cg;
+        }
+    }
+    public CanvasGroup CG
+    {
+        get
+        {
+            if (_Cg == null)    //尝试获取
+            {
+                _Cg = gameObject.GetComponent<CanvasGroup>();
+            }
+            if (_Cg == null)    //获取不到，就添加
+            {
+                _Cg = gameObject.AddComponent<CanvasGroup>();
+            }
+            return _Cg;
+        }
     }
 
     Action OnSelect;    //当选中
